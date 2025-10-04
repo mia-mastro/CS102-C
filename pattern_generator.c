6095cf4458112fa0fe4c5d3dab5d974a0a22c865
mia-mastro <mia.mastroianni@cooper.edu>
2025-10-03 21:38:11 -0400

    fixed small problem with checkerboard function

--- pattern_generator.c
+++ pattern_generator.c
@@ -84,7 +84,7 @@ void multiplicationTable(int size){
 void checkerboard(int size){
 	printf("Checkerboard:\n");
 	for(int i = 0; i < size; i++){
-		for(int j = 0; j < size; j++){
+		for(int j = 0; j < size * 2; j++){
 			if((i + j) % 2 == 0){
 				printf("*");
 			}else{
